a58d20ab844d419a3336500ad7e63b50310b085f
TusharSanisare <tsanisare@gmail.com>
2025-01-29 02:01:39 +0530

    Slug Url mapping to org done

--- src/main/java/com/url/UrlShortenerBackend/service/UrlMappingService.java
+++ src/main/java/com/url/UrlShortenerBackend/service/UrlMappingService.java
@@ -98,4 +98,20 @@ public class UrlMappingService {
 
   }
 
+  public UrlMapping getOriginalUrl(String shortUrl) {
+    UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+
+    if (urlMapping != null) {
+      urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+      urlMappingRepository.save(urlMapping);
+
+      ClickEvent clickEvent = new ClickEvent();
+      clickEvent.setClickDate(LocalDateTime.now());
+      clickEvent.setUrlMapping(urlMapping);
+      clickEventRepository.save(clickEvent);
+    }
+
+    return urlMapping;
+  }
+
 }
